94d91d96c58ecbde667e7710f13e4c33e0425112
janus926 <janus926@gmail.com>
2013-11-06 22:48:50 +0800

    Update design.

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 Still in design phase.
 # API
 ## reduler.connect(host, port)
-## reduler.add(job, args, date, [callback])
+## reduler.add(task, args, options, callback)
+options:
+- date: when to run the task, default is Date.now().
+- repeats: default is 1, use 0 for unlimited.
+- period: the time in milliseconds of repeat cycle, default 6000.
 ## reduler.remove(id)
-## reduler.run()
+## reduler.run(callback)
+# Event
+## success
+## failed
--- test.js
+++ test.js
@@ -1,9 +1,26 @@
 var reduler = require('./index');
 
-function jobMultiply (id, args) {
-  return args.a * args.b;
+var tasks = {
+  'multiply': function (a, b) {
+    return a * b;
+  }
 }
 
+var taskId;
+
 reduler.init(host, port);
-reduler.add(multiply, { a: 1, b: 2 }, new Date() + 10000);
-reduler.remove();
+reduler.add('multiply', { a: 1, b: 2 }, function(err, id) {
+  console.log('added:', err, id);
+  taskId = id;
+});
+
+reduler.run(function(id, task, args, callback) {
+  tasks[task](args);
+});
+
+reduler.on('success', function(id, task, args, result) {
+});
+reduler.on('failed', function(id, task, args, reason) {
+});
+
+reduler.remove(taskId);
